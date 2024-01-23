bfdd930ee464c4a965a99f1c02f91bb04d19ecb6
Thai Quang Minh <minhtq@notavn.com>
2024-01-23 15:18:49 +0700

    Show-up-animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <nav>
+          <img className='logo' src='assets/images/logo.png' alt='' />
+          <ul>
+            <li>
+              <a href='#'>Features</a>
+            </li>
+            <li>
+              <a href='#'>How it works</a>
+            </li>
+            <li>
+              <a href='#'>Privacy</a>
+            </li>
+          </ul>
+
+          <div>
+            <a href='#' className='login-btn'>Log in</a>
+            <a href='#' className='btn'>Download App</a>
+          </div>
+        </nav>
+
+        <div className='content'>
+          <h1>Make<br />New Friends</h1>
+          <p>Socially is a new social media platform to make new friends online in the world. Now lets explore all its amazing features.</p>
+          <button className='btn'>Join Now</button>
+        </div>
+        <img className='pic' src='assets/images/pic.png' />
+      </div>
     </div>
   );
 }
